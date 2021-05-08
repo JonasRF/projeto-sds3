627a5af70216f5ca211149df44264725aad12c8e
Jonas Ribeiro <jonasribeiro41@yahoo.com.br>
2021-05-08 15:26:24 -0300

    Static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,35 @@
+import BarChat from 'components/BarChat';
 import DataTable from 'components/DataTable';
+import DonutChat from 'components/DonutChart';
 import Footer from 'components/Footer';
 import NavBar from 'components/NavBar';
 
 function App() {
   return (
     <>
-    <NavBar/>
-    <div className="container">
-      <h1 className="text-primary">Olá mundo!</h1>
-      <DataTable/>
-    </div>
-    <Footer/>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary py-3">Dashboard de vendas</h1>
+
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de sucesso</h5>
+            <BarChat />
+          </div>
+
+            <div className="col-sm-6">
+              <h5 className="text-center text-secondary">Todas as Vendas</h5>
+              <DonutChat />
+            </div>      
+        </div>
+         
+         <div className="py-3">
+           <h2 className="text-primary">Todas Vendas</h2>
+         </div>
+
+        <DataTable />
+      </div>
+      <Footer />
     </>
   );
 }
